23b58631b8f743af64b4abaa5968fc0de4c0c148
LitvinovaOlha1905 <130159769+LitvinovaOlha1905@users.noreply.github.com>
2023-11-26 16:19:55 +0100

    new hw_261123

---
+++ hw/hw_261123.sql
@@ -0,0 +1,28 @@
+-- В рамках БД "песочница" напишите след/запросы:
+
+-- 1. Вывести самый дорогой товар из категории 4.
+
+   SELECT * 
+   FROM Products
+
+   WHERE CategoryID = 4
+   ORDER BY Price DESC
+
+-- 2. Вывести страны поставщиков, кроме поставщика 1.
+
+   SELECT Country 
+   FROM Suppliers
+
+   WHERE SupplierID != 1
+
+-- 3. Вывести клиентов из Germany, имена которых начинаются на t.
+
+   SELECT * FROM Customers
+
+   WHERE Country = "Germany"
+   AND CustomerName LIKE "t%"
+
+-- 4. Вывести телефоны перевозчиков.
+
+   SELECT Phone
+   FROM Shippers
